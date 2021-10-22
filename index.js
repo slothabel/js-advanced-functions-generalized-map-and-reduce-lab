e41a0f28b1d5b9b32b654332633045eaa47fd9a5
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-22 00:47:36 -0400

    commit tree a9fd054e52f807aea9855e61bcc351e28fd96091

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Add your functions here
+
+function map(arr, callback) {
+    let negs = []
+    for(var i = 0; i < arr.length; i++) {
+        //for each element in the array, save it to currElmt
+        const currElmt = arr[i]
+        //add the return value of calling the current callback function along with the current element
+        negs.push(callback(currElmt))
+    }      
+    return negs
+}
+
+function reduce(arr, callback, startingValue) {
+    //declare variable, currently its value is just undefined
+    let total;
+    //if startingValue is true/is not 0
+    if (startingValue) {
+        //then total takes the startingValue's value
+      total = startingValue;
+      for (let i = 0; i < arr.length; i++) {
+          //then total becomes the result of the callback function 
+          //using the current number being iterated and the current value in total
+        total = callback(arr[i], total);
+      }
+      //return total(part of the "if startvalue is true/not0" branch)
+      return total;
+      //else, if startvalue is false/NOT 0
+    } else {
+        //make the total whatever number is in the first element 
+        //of the source array passed in originally
+      total = arr[0];
+      for (let i = 1; i < arr.length; i++) {
+        //then total becomes the result of the callback function 
+        //using the current number being iterated and the current value in total  
+        total = callback(arr[i], total);
+      }
+      return total;
+    }
+  }
